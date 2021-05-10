888ae37faec732f52d7194e2fc7d721ff37e7b38
KrystopherWeeton <krystopherweeton@gmail.com>
2021-05-10 15:12:18 -0700

    Move heuristic experiment into exp hierarchy

--- exp.py
+++ exp.py
@@ -2,13 +2,14 @@
 import click
 
 from experiments.size import size
-
+from experiments.heuristic import heuristic
 
 @click.group()
 def run():
     pass
 
 run.add_command(size)
+run.add_command(heuristic)
 
 if __name__ == "__main__":
     run()
