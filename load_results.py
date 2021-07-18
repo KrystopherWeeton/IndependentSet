66e37feedee365f3ac5f111c4ba12fd6a4363590
KrystopherWeeton <krystopherweeton@gmail.com>
2021-07-18 15:15:18 -0700

    Move results examination over to click for more option control

--- load_results.py
+++ load_results.py
@@ -1,13 +1,15 @@
 import sys
+import click
 import os
 from util.storage import load_from_path
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
+
+@click.command()
+@click.option("--path", required=True)
+def run(path):
+    if path is None:
         print("Error: No valid path provided. Results cannot be loaded.")
         sys.exit(1)
-    
-    path: str = sys.argv[1]
 
     if not os.path.isfile(path):
         click.echo("Error: Invalid path provided")
@@ -15,3 +17,6 @@ if __name__ == "__main__":
 
     result = load_from_path(path)
     print(f"The requested result has been loaded from {path}. It may be accessed through the `results` object")
+
+if __name__ == "__main__":
+    run()
