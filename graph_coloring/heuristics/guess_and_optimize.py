369f1213b9cfa75151cfb4c93b7d4070c0856b07
ppaleja <ppaleja@ucsd.edu>
2021-07-20 14:27:55 -0700

    finished guess_and_optimize heuristic. Still need to test allllll the stuff though

--- graph_coloring/heuristics/guess_and_optimize.py
+++ graph_coloring/heuristics/guess_and_optimize.py
@@ -14,33 +14,54 @@ class GuessAndOptimize(GraphColoringHeuristic):
         super(GuessAndOptimize, self).__init__()
         self.initial_guess = guess
 
+    def _optimize(self, iterations: int):
+        for i in range(iterations):
+            # Find the most conflicted node
+            most_conflicted: int = self.solution.most_collisions_node()
+
+            # Recolor this node
+            self.solution.color_node(most_conflicted, self.solution.best_recoloring(most_conflicted))
+
     def _run_heuristic(self):
 
         n: int = len(self.G)
 
-        #TODO: do we need stirling table still?
+        # TODO: do we need stirling table still?
 
         # Make initial guess
         b: float = 1 / (1 - nx.density(G))
         k: int = self.initial_guess if self.initial_guess != 0 else (n / math.log(n, b))
+        iterations: int = n * k
+        tries: int = n
 
         # Upper bound phase
         while True:
 
             # Set the initial solution with a random partitioning into guess # of sets
             self.solution.set_coloring_with_color_classes(generate_random_color_partition(self.G, k))
+            # Now try to optimize for iterations
+            self._optimize(iterations)
+            # If we got to a valid solution, don't need to go up any more
             if self.solution.get_num_conflicting_edges() == 0 or len(self.solution.color_to_nodes):
                 break
             k = k + 1
 
         # Initialize the above as the best one we have so far.
-        best_conflicts: int = self.solution.num_conflicting_edges
-        best_labelling: Dict[int, int] = copy.copy(self.solution.node_to_color)
 
         while True:
+            # Question: Do we need to know the conflict here? Could be interesting if we could do
+            best_conflicts: int = self.solution.num_conflicting_edges
+            best_labelling: Dict[int, int] = copy.copy(self.solution.node_to_color)
+
             # Try coloring with one less color
             k = k - 1
-            # Save old partition
-            old_partition: Dict[int, int] = copy.copy(self.solution.node_to_color)
-            # TODO: I guess we kinda have to make sure that there's no conflicts...
             self.solution.set_coloring_with_color_classes(generate_random_color_partition(self.G, k))
+
+            self._optimize(iterations)
+
+            # If we ran into conflicts on this one, lets just revert back to the last one.
+            if self.solution.get_num_conflicting_edges() != 0:
+                self.solution.set_coloring_with_node_labels(best_labelling)
+                break
+
+        return
--- util/models/graph_coloring_tracker.py
+++ util/models/graph_coloring_tracker.py
@@ -41,15 +41,15 @@ class GraphColoringTracker(Solution):
         self.num_neighbor_colors: np.array = np.array([defaultdict(int)] * len(self.G))
         self.num_conflicting_edges: int = self.G.number_of_edges()
 
-    """
-    Takes care of initializing:
-        saturation,
-        collisions at v
-        total # of collisions
-        neighboring colors of v
-    """
-    def init_tables(self):
 
+    def init_tables(self):
+        """
+        Takes care of initializing:
+            saturation,
+            collisions at v
+            total # of collisions
+            neighboring colors of v
+        """
         self.init_saturation()
         self.init_collisions_and_nn_colors()
 
@@ -122,8 +122,14 @@ class GraphColoringTracker(Solution):
             self.num_conflicting_edges -= int(self.node_to_color[neighbor] == old_color)
             self.num_conflicting_edges += int(self.node_to_color[neighbor] == color)
 
+    # TODO: Implement as priority queue
     def most_collisions_node(self) -> int:
+        """
+        :return: Node that is most collisioned
+        """
         return self.collisions_at.argmax()
+
+    # TODO: Add way to make recoloring loss function modular
 
     # Complexity: O()
     def most_distinctly_saturated_node(self) -> int:
@@ -131,7 +137,12 @@ class GraphColoringTracker(Solution):
         # return np.count_nonzero(self.num_neighbor_colors, axis=0).argmax()
         return self.saturation.argmax()
 
+
     def best_recoloring(self, node: int) -> int:
+        """
+        :param node:
+        :return: color that is best for this node
+        """
         # We want to pick the color that causes the least amount of conflicts for this node
         # That would be the argmin of this particular row? I think
         return min(self.num_neighbor_colors[node], key=self.num_neighbor_colors[node].get)
