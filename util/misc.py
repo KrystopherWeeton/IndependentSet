ad22c3a1eb621faaa7c0f840a331288346b00669
KrystopherWeeton <krystopherweeton@gmail.com>
2021-08-02 14:19:20 -0700

    Add in pull_values

--- util/misc.py
+++ util/misc.py
@@ -1,3 +1,4 @@
+from typing import List
 
 def round_all_values(M: [[float]], num_points: int) -> [[float]]:
     return [
@@ -12,3 +13,14 @@ def validate(predicate: bool, message: str):
     """ Validates the predicate evaluates to true, printing the message provided if it doesn't """
     if not predicate:
         raise Exception(message)
+
+
+def pull_values(d: dict, *args) -> List:
+    """
+    Pulls values from the dictionary provided
+        `d` the dictionary to pull values from
+        `args` the keys to pull from the dictionary
+    NOTE: Validates keys beforehand and throws an error if a key isn't found.
+    """
+    validate(all(args) in d.keys(), f"Can't pull {args} from {d} because a key wasn't found.")
+    return [d[key] for key in args]
