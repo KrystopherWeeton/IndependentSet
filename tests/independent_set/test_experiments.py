24ba685c82e86cf4bde05e21ecf9c4b14ab68e62
KrystopherWeeton <krystopherweeton@gmail.com>
2021-09-08 16:53:04 -0700

    Add in 'heuristic' test

--- tests/independent_set/test_experiments.py
+++ tests/independent_set/test_experiments.py
@@ -1,5 +1,5 @@
-import subprocess
 import unittest
+from subprocess import CompletedProcess, run
 
 
 class TestExperiment(unittest.TestCase):
@@ -8,18 +8,32 @@ class TestExperiment(unittest.TestCase):
         pass
 
 
+    def _assert_success(self, response: CompletedProcess):
+        assert response.returncode == 0, f"Response Code: {response.returncode}, stdout='{response.stdout}'"
+
+
     def test_successive_augmentation(self) -> None:
         """
         Test successive augmentation compiles and runs
         """
-        response: subprocess.CompletedProcess = subprocess.run(
-            args="./exp.py ind-set sa -n 500 --num-trials 1 --transient",
+        response: CompletedProcess = run(
+            args="./exp.py ind-set sa -n 100 --num-trials 1 --transient",
             capture_output=True,
             shell=True,
         )
-        assert response.returncode == 0, f"Response Code: {response.returncode}, stdout='{response.stdout}'"
+        self._assert_success(response)
 
 
+    def test_heuristic(self) -> None:
+        """
+        Test that heuristic experiment compiles and runs
+        """
+        response: CompletedProcess = run(
+            args="./exp.py ind-set heuristic -n 100 --num-trials 1 --transient",
+            capture_output=True,
+            shell=True,
+        )
+        self._assert_success(response)
 
 if __name__ == "__main__":
     unittest.main()
