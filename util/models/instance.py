9e3d5a0fe63cea3b3cd1c7b064b81d1f55b765b9
KrystopherWeeton <krystopherweeton@gmail.com>
2022-01-06 13:56:12 -0800

    Create instance class

---
+++ util/models/instance.py
@@ -0,0 +1,23 @@
+from typing import Callable
+
+from util.models.solution import Solution
+
+Metric = Callable[[Solution], float]
+
+class Instance:
+
+    metric: Metric = None
+
+    def __init__(self):
+        self.metric = None
+        pass
+
+    def set_metric(self, metric: Metric):
+        self.metric = metric
+
+    def neighbors(self, v: Solution) -> List[Solution]:
+        raise NotImplementedError("Create instance subclass")
+
+    @staticmethod
+    def generate_random_instance(n: int) -> "Instance":
+        raise NotImplementedError("Create instance subclass")
