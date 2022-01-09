fadf60173ebb2906481f5a03ef2ec41ef0d9d199
KrystopherWeeton <krystopherweeton@gmail.com>
2022-01-09 14:22:08 -0800

    .

---
+++ c_code/lib/README.md
@@ -0,0 +1,14 @@
+### Library List
+
+**Cereal**: Used to serialize objects to store experimental results
+
+[Documentation](https://uscilab.github.io/cereal/quickstart.html)
+
+
+<br />
+
+**Boost Graph Library**: Used for graph management
+
+[Documentation](https://www.boost.org/doc/libs/1_77_0/libs/graph/doc/index.html)
+
+- '/boost' directory holds all executable code. 'boost_1_66_0' holds documentation, license, etc.
--- c_code/main.cpp
+++ c_code/main.cpp
@@ -1,5 +1,9 @@
 #include <fstream>
 #include "lib/cereal/archives/binary.hpp"
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,5 +14,12 @@ int main() {
         int x = 1;
         archive(x);
     }
+
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+
     return 0;
 }
